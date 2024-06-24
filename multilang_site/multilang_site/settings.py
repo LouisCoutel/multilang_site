0985d1eb82e8e5d3e05f7ac2757dcf62a13880d2
LouisCoutel <louiscoutel75@gmail.com>
2024-06-24 16:17:37 +0200

    build(dependencies): clean up project dependencies

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_htmx',
+    'django_extensions',
     'modeltranslation',
     'template_partials',
     'pgvector',
@@ -90,10 +91,9 @@ LANGUAGES = (
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'multilang_db',
         'USER': 'admin',
